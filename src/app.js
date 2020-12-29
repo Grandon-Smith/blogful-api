d581946be77cf50eecc1afd676270a49adcf7576
Grandon Smith <grandon.smith@yahoo.com>
2020-12-29 17:19:06 -0500

    refactoring with new database migration and articles, comments and user input endpoints

--- src/app.js
+++ src/app.js
@@ -5,13 +5,16 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const articlesRouter = require('./articles/articles-router')
+const usersRouter = require('./users/users-router')
+const commentsRouter = require('./comments/comments-router')
 
 const app = express()
 
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common'))
 app.use(cors())
 app.use(helmet())
-
+app.use('/api/users', usersRouter)
+app.use('/api/comments', commentsRouter)
 app.use('/api/articles', articlesRouter)
 
 app.get('/', (req, res) => {
